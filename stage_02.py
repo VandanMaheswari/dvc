ad9b76e0ca299dbe1c301c48b94b86a5201bdadb
vandan maheshwari <vandanmaheshwari9407@gmail.com>
2023-06-28 17:08:57 +0530

    file updated

--- stage_02.py
+++ stage_02.py
@@ -4,3 +4,8 @@ with open("artifacts.txt" , 'r') as f:
     print(text)
     print("it is a end of stage 2")
     print("i am going to add stage 3")
+    print("checking the changes in dvc.lock")
+    
+
+
+# dvc.lock has all the information related to changes and all the meta deta 
---
+++ stage_03.py
@@ -0,0 +1,9 @@
+with open("artifacts.txt","r") as f:
+    text=f.read()
+    
+with open("artifacts.txt","w") as f:
+    text=f.write(text+"i have added one line")
+    
+    
+print(text)
+print("it is a end of stage 3")
